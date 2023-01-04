e65fa3495597f422cad75858b38c7bb9c7c5a972
Nyktor <nyktorrodriguez@gmail.com>
2023-01-04 03:57:42 +0100

    Added gameplay
    bananas

--- C/Tic Tac Cock/TicTacCock.c
+++ C/Tic Tac Cock/TicTacCock.c
@@ -16,35 +16,53 @@ char getWinner();
 //var & const declaration
 const char PLAYER1 = 'X';
 const char PLAYER2 = 'O';
-#define rows 3
-#define cols 3
-char board[rows][cols];
+char board[3][3];
 
 
 
 //main function
 int main(){
 
-    resetBoard();
-    printBoard();
-    while(emptySquares() != 0){
-        selectSquare(PLAYER1);
-        selectSquare(PLAYER2);
-    }
+    char winner = ' ';
+    char answer = 'Y';
 
-    if(getWinner() == ' '){
-        printf("\nIt's a tie!");
-    }else{
-        printf("\nPlayer %c wins!", getWinner());
-    }
+    do{
+        resetBoard();
+        while(winner == ' ' && emptySquares() != 0){
+            printBoard();
+            selectSquare(PLAYER1);
+            winner = getWinner();
+
+            if(getWinner() != ' ' || emptySquares() == 0){
+                break;
+            }
+
+            printBoard();
+            selectSquare(PLAYER2);
+            winner = getWinner();
+
+            if(getWinner() != ' ' || emptySquares() == 0){
+                break;
+            }
+        }
+        
+        if(winner == ' '){
+            printf("\n It's a tie!");
+        }else{
+            printf("Player %c is the winner!", winner);
+        }
+        printf("\nWould you like to play again? (Y/N): ");
+        scanf("%c", &answer);
+
+    }while(answer == 'Y');
 
     return 0;
 }
 
 //functions and actions
 void resetBoard(){
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
 
             board[i][j] = ' ';
 
@@ -53,7 +71,7 @@ void resetBoard(){
 }
 
 void printBoard(){
-    printf("  %c | %c | %c", board[0][0], board[0][1], board[0][2]);
+    printf("\n  %c | %c | %c", board[0][0], board[0][1], board[0][2]);
     printf("\n ---|---|---\n");
     printf("  %c | %c | %c", board[1][0], board[1][1], board[1][2]);
     printf("\n ---|---|---\n");
@@ -64,62 +82,59 @@ void selectSquare(char player){
     int x, y;
     printf("\nPlayer %c's turn!", player);
     do{
-    printf("\nSelect a row number (1-3): ");
-    scanf("%d", &x);
-    x--;
+        printf("\nSelect a row number (1-3): ");
+        scanf("%d", &x);
+        x--;
 
-    printf("\nSelect a column number (1-3): ");
-    scanf("%d", &y);
-    y--;
+        printf("Select a column number (1-3): ");
+        scanf("%d", &y);
+        y--;
 
-    if(x >-1 && x < 3 && y > -1 && y < 3){
         if(board[x][y] == ' '){
             board[x][y] = player;
+            break;
         }else{
             printf("\n That square is already taken!");
         }
-    }else{
-        printf("\n That's not a valid square!");
-    }
 
-    }while(x >-1 && x < 3 && y > -1 && y < 3 && board[x][y] == ' ');
+    }while(board[x][y] == ' ');
    
 }
 
 int emptySquares(){
     int empty = 9;
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
-            if(board[i][j] == ' '){
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if(board[i][j] != ' '){
                 empty--;
             }
         }
     }
-
     return empty;
 }
 
 char getWinner(){
     //check rows
-    for(int i = 0; i < rows; i++){
+    for(int i = 0; i < 3; i++){
         if(board[i][0] == board[i][1] && board[i][0] == board[i][2]){
             return board[i][0];
         }
     }
 
     //check columns
-    for(int i = 0; i < rows; i++){
+    for(int i = 0; i < 3; i++){
         if(board[0][i] == board[1][i] && board[0][i] == board[2][i]){
             return board[0][i];
         }
     }
 
     //check diagonals
-        if(board[0][0] == board[1][1] && board[0][0] == board[2][2]){
-            return board[0][0];
-        }else if(board[0][2] == board[1][1] && board[0][2] == board[2][0]){
-            return board[0][2];
-        }
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2]){
+        return board[0][0];
+    }
+    if(board[0][2] == board[1][1] && board[0][2] == board[2][0]){
+        return board[0][2];
+    }
 
     //it's a draw
     return ' ';
