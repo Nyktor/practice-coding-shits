5a6d89eff8e452162d3082628fde8531116d2d56
Nyktor <nyktorrodriguez@gmail.com>
2023-01-09 02:09:34 +0100

    Finished attack
    bananas

--- C/MiniPVP/MiniPVP.c
+++ C/MiniPVP/MiniPVP.c
@@ -24,11 +24,10 @@ typedef struct {
     char name[NAME_MAX_LENGTH];
     int hp;
     int dmg;
-    bool isAlive;
 } Player;
 
 /* Global variables */
-int round;
+int roundNumber;
 int currentPlayerId;
 Player player1;
 Player player2;
@@ -38,6 +37,8 @@ void resetGame();
 void selectNames();
 void printHP();
 void startTurn();
+void attack(int);
+Player getPlayer(int);
 int checkWinner();
 
 
@@ -54,8 +55,10 @@ int main(){
         while(checkWinner() == 0){
             printHP();
             startTurn();
-
         }
+        printf("\n\n*********************************");
+        printf("\n%s WINS!! uwu", getPlayer(checkWinner()).name);
+        printf("\n*********************************");
 
         printf("\nDo you want to play again? (Y/N): ");
         scanf("%c", &playAgain);
@@ -70,14 +73,12 @@ int main(){
 
 /* Reset all parameters of the game */
 void resetGame(){
-    round = 0;
+    roundNumber = 0;
     //Resets players' stats
     strncpy(player1.name, "", NAME_MAX_LENGTH);
     strncpy(player2.name, "", NAME_MAX_LENGTH);
     player1.hp = MAXHP;
     player2.hp = MAXHP;
-    player1.isAlive = true;
-    player2.isAlive = true;
     currentPlayerId = 0;
 }
 
@@ -101,7 +102,7 @@ void selectNames(){
 /* Method used to print both players' HP*/
 void printHP(){
     //Player 1
-    printf("\n%s [", player1.name);
+    printf("\n\n%s [", player1.name);
     for(int i = 0; i < player1.hp / 5; i++){
         printf("||||| ");
     }
@@ -122,19 +123,50 @@ void printHP(){
 
 /* Method that is called each turn */
 void startTurn(){
+    char action;
     if(currentPlayerId == 0){
         //IE: game at the start, chooses a random player
         srand(time(0));
         currentPlayerId = rand() % 2 + 1;
     }
+    printf("\n\nYour turn, %s!", getPlayer(currentPlayerId).name);
+    printf("\nWhat do you want to do? \nAttack: \"A\"\nChoose: ");
+    scanf("%c", &action);
+    scanf("%c");
+    action = toupper(action);
+
+    if(action == 'A'){
+        //Attacks the other player and changes the current player ID
+        attack(currentPlayerId % 2 + 1);
+        currentPlayerId = currentPlayerId % 2 + 1;
+    }
 }
 
 /* Checks if there's a winner, returns its ID if is there one, returns 0 if not*/
 int checkWinner(){
-    if(!player1.isAlive){
+    if(player1.hp <= 0){
         return 2;
-    }else if(!player2.isAlive){
+    }else if(player2.hp <= 0){
         return 1;
     }
     return 0;
+}
+
+/* Substracts a random amount of HP from the selected character*/
+void attack(int id){
+    srand(time(0));
+    if(id == 1){
+        player1.hp -= ((rand() % (MAXDMG - MINDMG + 1)) + MINDMG);
+    }else if (id == 2){
+        player2.hp -= ((rand() % (MAXDMG - MINDMG + 1)) + MINDMG);
+    }
+}
+
+/* Gets player by ID */
+Player getPlayer(int id){
+    if(id == 1){
+        return player1;
+    }else if(id == 2){
+        return player2;
+    }
 }
