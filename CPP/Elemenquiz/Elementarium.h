5325dab462098a494a9d1d09ad63eb8451c8d9ed
Nyktor <nyktorrodriguez@gmail.com>
2023-01-14 04:54:04 +0100

    Started C++
    bananas

--- CPP/Elemenquiz/Elementarium.h
+++ CPP/Elemenquiz/Elementarium.h
@@ -1,4 +1,17 @@
+#include <iostream>
+#include <string>
+enum Element{IGNIS, ELEKTRO, HYDROS, VITAS, TERRA, METALLUM, VENTUS, GLAKIES, LUX, UMBRA};
+enum Aether{STRIKING, FLOWING, UNBREAKABLE, HOWLING, POLAR};
+
+using std::string;
+
 class Elementarium{
 	private:
-		std::string name;
+		string name;
+		int scores[10];
+		Element eAffinity;
+		Aether aAffinity;
+	public:
+		void setName(string);
+		
 };
